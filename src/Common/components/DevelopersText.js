b19e602f28ab875c20891d22bc3ac027c73b1b80
= <garvitgalgat@gmail.com>
2020-12-31 13:46:27 +0530

    rel= noreferrer added

--- src/Common/components/DevelopersText.js
+++ src/Common/components/DevelopersText.js
@@ -5,16 +5,16 @@ function DevelopersText() {
 	return (
 		<div className="Text1 dev1">
 			<p className="text1stuff">Developers</p>
-			<a target="_blank" className="devlink" href="https://github.com/gg-dev-05">
+			<a target="_blank" rel="noreferrer" className="devlink" href="https://github.com/gg-dev-05">
 				Garvit Galgat
 			</a>
-			<a target="_blank" className="devlink" href="https://github.com/khushi935">
+			<a target="_blank" rel="noreferrer" className="devlink" href="https://github.com/khushi935">
 				Khushi
 			</a>
-			<a target="_blank" className="devlink" href="https://github.com/kushaangowda">
+			<a target="_blank" rel="noreferrer" className="devlink" href="https://github.com/kushaangowda">
 				Kushaan Gowda
 			</a>
-			<a target="_blank" className="devlink last" href="https://github.com/vaibhavviking">
+			<a target="_blank" rel="noreferrer" className="devlink last" href="https://github.com/vaibhavviking">
 				Vaibhav Chandra
 			</a>
 			<LoginButton />
