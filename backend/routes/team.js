cd850892fb493fc94d6042c271894e6362a8a201
= <garvitgalgat@gmail.com>
2021-01-02 21:08:58 +0530

    team routes added

--- backend/routes/team.js
+++ backend/routes/team.js
@@ -2,23 +2,7 @@ const router = require('express').Router()
 const Team = require('../models/team');
 
 
-
-
-
-router.route('/add').post((req, res) => {
-    let name = req.body.name;
-    let members = [];
-    let team = new Team({
-        name: name,
-        members: members
-    })
-
-    team.save().then(() => {
-        console.log("ADDED")
-        res.send("DONE")
-    })
-})
-
+// Get Route for teams
 router.route('/').get((req, res) => {
     Team.find({}, (err, result) => {
 
@@ -60,4 +44,74 @@ router.route('/:teamID').get(
             }
         })
     })
+
+router.route('/add').post((req, res) => {
+    let name = req.body.name;
+    let members = req.body.members;
+    let team = new Team({
+        name: name,
+        members: members
+    })
+
+    console.log(team)
+
+    team.save((err, result) => {
+        if (err) {
+            res.send({
+                "error": err.message
+            })
+        }
+        else {
+            res.send({
+                "result": `Created: ${result}`
+            })
+        }
+    })
+})
+
+router.route('/delete/:teamID').delete((req, res) => {
+    let teamID = req.params.teamID;
+    Team.findByIdAndDelete(teamID, (err, result) => {
+        if (err) {
+            console.log(err)
+            res.send({
+                "error": err
+            })
+        }
+        else {
+            res.send({
+                "result": `Deleted: ${result}`
+            })
+        }
+    })
+})
+
+router.route('/update/:teamID').put((req, res) => {
+    let teamID = req.params.teamID;
+    console.log(teamID)
+    let members = req.body.members;
+    let name = req.body.name;
+
+    let team = {}
+    if (name)
+        team['name'] = name;
+    if (members)
+        team['members'] = members
+
+    Team.findByIdAndUpdate(teamID, team, (err, result) => {
+        if (err) {
+            console.log(err)
+            res.send({
+                "error": err
+            })
+        }
+        else {
+            console.log(result)
+            res.send({
+                "result": `Updated: ${result}`
+            })
+        }
+        console.log("DONE")
+    })
+})
 module.exports = router
