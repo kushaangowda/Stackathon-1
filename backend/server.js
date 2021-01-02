49306a26baccbafa57575d875149e7698e53dc8d
vaibhav <chandravaibhav65@gmail.com>
2021-01-02 12:48:20 +0530

    db linked

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGO_URI;
+
+mongoose.connect(uri, {useNewUrlParser: true , useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open',()=>{
+  console.log('MongoDB database linked successfully!');
+})
+
+// app.use('/exercises',exercisesRouter);
+// app.use('/users',userRouter);
+
+
+
+app.listen(port,()=>{
+  console.log("Server is running at port : " , port);
+})
+console.log(uri)
