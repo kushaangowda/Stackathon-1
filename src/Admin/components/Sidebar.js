4840e3b277533cc0ed556b669388b5b0ff2c60c1
Kushaan Gowda <kushaangowda@yahoo.com>
2021-01-05 22:48:24 +0530

    fixed login logout

--- src/Admin/components/Sidebar.js
+++ src/Admin/components/Sidebar.js
@@ -31,7 +31,7 @@ function Sidebar() {
 							if (item.title === "Log Out") {
 								return (
 									<li key={index} className={item.CName}>
-										<a href="https://dev-f-rf7g-f.us.auth0.com/v2/logout?returnTo=http%3A%2F%2Flocalhost%3A3000">
+										<a href={item.path}>
 											{item.icons}
 											<span>{item.title}</span>
 										</a>
--- src/Admin/components/SidebarData.js
+++ src/Admin/components/SidebarData.js
@@ -50,6 +50,7 @@ export const SidebarData = [
 	},
 	{
 		title: "Log Out",
+		path: "https://dev-f-rf7g-f.us.auth0.com/v2/logout?returnTo=http%3A%2F%2Flocalhost%3A3000",
 		icons: <AiIcons.AiOutlineLogout />,
 		CName: "nav-text",
 	},
