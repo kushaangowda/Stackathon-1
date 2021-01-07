0803445f9996e082487c867292086e6bf171aafd
Kushaan Gowda <kushaangowda@yahoo.com>
2021-01-07 17:39:28 +0530

    added preloader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { CommonScreen } from "./Common/CommonScreen";
+import { LoadingScreen } from "./Common/LoadingScreen";
 import { Admincommon } from "./Admin/Admincommon";
+import { useAuth0 } from "@auth0/auth0-react";
+
 function App() {
+	const { isLoading } = useAuth0();
+
+	if (isLoading)
+		return (
+			<div className="App">
+				<LoadingScreen />
+			</div>
+		);
+
 	return (
 		<div className="App">
 			<CommonScreen />
