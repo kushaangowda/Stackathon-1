d07496ac328e2bef9c7647855361ba3a17140f5a
Kushaan Gowda <kushaangowda@yahoo.com>
2021-01-10 10:04:12 +0530

    fixed some stuff

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,10 @@ const cors = require('cors');
 const employeeRouter = require('./routes/Employee');
 const leaveRequestRouter = require('./routes/leaveRequest');
 const payrollRequestRouter = require('./routes/payrollRequest');
-const teamRouter = require('./routes/team')
-const attendanceRouter = require('./routes/Attendance')
-const taskRouter = require('./routes/task')
+const teamRouter = require('./routes/team');
+const attendanceRouter = require('./routes/Attendance');
+const taskRouter = require('./routes/task');
+const documentRouter = require('./routes/Document');
 require('dotenv').config();
 
 
@@ -31,6 +32,7 @@ app.use('/leaverequest', leaveRequestRouter);
 app.use('/payrollrequest', payrollRequestRouter);
 app.use('/attendance', attendanceRouter);
 app.use('/task', taskRouter);
+app.use('/document', documentRouter);
 
 app.listen(port, () => {
   console.log("Server is running at port : ", port);
