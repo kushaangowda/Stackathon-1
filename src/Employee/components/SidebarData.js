4235bba8a5b8421950a11f932e1542fd178a8c36
= <garvitgalgat@gmail.com>
2021-01-23 23:06:54 +0530

    somethn

--- src/Employee/components/SidebarData.js
+++ src/Employee/components/SidebarData.js
@@ -32,7 +32,7 @@ export const SidebarData = [
 	},
 	{
 		title: "Log Out",
-		path: "https://dev-f-rf7g-f.us.auth0.com/v2/logout?returnTo=http%3A%2F%2Flocalhost%3A3000",
+		path: "https://dev-f-rf7g-f.us.auth0.com/v2/logout?returnTo=https%3A%2F%2Fstackathon.netlify.app",
 		icons: <AiIcons.AiOutlineLogout />,
 		CName: "nav-text",
 	},
