d2a7ac827db40cf146fa4ce5658b0003d7c57daf
vaibhav <chandravaibhav65@gmail.com>
2021-01-23 16:52:05 +0530

    error notification 2

--- src/Admin/components/EmployeeCard.js
+++ src/Admin/components/EmployeeCard.js
@@ -30,37 +30,16 @@ export default function EmployeeCard(props) {
 			});
 	};
 
-	const [attendanceMarked, setAttendanceMarked] = useState(false);
-	const [isError, setIsError] = useState(false);
-	const markAttendance = () => {
-		axios.get(`https://api-stackathon.herokuapp.com/attendance/${props.details._id}/create`)
-			.then((res) => {
-				setAttendanceMarked(true)
-				createNotification({
-					title: "Success",
-					message: "Attendance successfully marked!",
-					type: "success",
-					time: 3000
-				})
-			})
-			.catch((err) => {
-				createNotification({
-					title: "",
-					message: err.message,
-					type: "warning",
-					time: 1000
-
-				})
-				setAttendanceMarked(false);
-				setIsError(true)
-				createNotification({
-					title: ":(",
-					message: "Something went wrong, Please try again later!",
-					type: "danger",
-					time: 5000
-				})
-			})
-	}
+	const attendance = () => {
+		if (isError) return <h6>Something Went Wrong</h6>;
+		if (attendanceMarked) return <h6>Attendance for today already marked</h6>;
+		else
+			return (
+				<button className="btn btn-success" onClick={markAttendance}>
+					Mark Attendance
+				</button>
+			);
+	};
 
 	return (
 		<tr className="emp-card">
